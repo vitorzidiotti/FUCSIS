ed3be1c1c14e2ee98577c53a7ba36ca17ee8898e
Vitor Antonio Zidiotti de Souza <160920505+vitorzidiotti@users.noreply.github.com>
2025-11-10 17:58:16 -0300

    Update admin_estoque_routes.py

--- api/routes/admin_estoque_routes.py
+++ api/routes/admin_estoque_routes.py
@@ -2,6 +2,6 @@ from flask import Blueprint, render_template
 
 estoque_bp = Blueprint('estoque', __name__)
 
-@estoque_bp.route('/estoque')
-def estoque():
+@estoque_bp.route('/estoque_mov')
+def estoque_mov():
     return render_template('admin_estoque.html')
